47e032642e0129e24a5e36c4cc491e0f115548ba
WilliamCDL <42789470+WilliamCDL@users.noreply.github.com>
2019-04-17 20:02:20 -0300

    Add files via upload

--- TestesDiif.py
+++ TestesDiif.py
@@ -1,96 +1,7 @@
 #!/usr/bin/python
-import ast
 import os
 from pydriller import RepositoryMining
-#variaveis globais para salvar o nome da func e a lista das funcs desejadas
-NomeDeFunc=""
-NomeDeClass=""
-#variavel para guardar a quantidade de funcoes numa classe
-Contador = 0
-DicionarioDeNomedeFunc = {}
-
-class MyCustomVisitor(ast.NodeVisitor):
-    
-    def __init__(self):
-        self.counter = 0
-        
-        global NomeDeFunc
-        global Contador
-        global NomeDeClass
-        global DicionarioDeNomedeFunc
-        Contador = 0
-        self.commit_message = ''
-        self.except_found = False
-        NomeDeFunc=""
-        NomeDeClass=""
-        DicionarioDeNomedeFunc.clear()
-        
-        
-    def visit_FunctionDef(self, node):
-        global NomeDeFunc
-        global Contador
-        global NomeDeClass
-
-
-        if Contador == 0 :
-            NomeDeClass = ""
-            NomeDeFunc = ""
-        
-        if Contador >= 1 :
-            Contador -= 1
-        
-        #Salvando o nome da funcao e seus parametros
-        
-            
-        NomeDeFunc = NomeDeClass + node.name+"("
-        for Parametros in node.args.args:
-            if Parametros == node.args.args[-1] :
-                NomeDeFunc+=Parametros.arg
-            else :
-                NomeDeFunc+=Parametros.arg+","
-        NomeDeFunc+=")" 
-                    
-            
-
-        #necessario para poder visitar os filhos
-        super(MyCustomVisitor, self).generic_visit(node)
-
-       
-
-    def visit_ExceptHandler(self, node):
-        global NomeDeFunc
-        global NomeDeClass
-        global Contador
-        global DicionarioDeNomedeFunc
-
-        #caso a lista tenha tamanho 1
-        if node.body[0] == node.body[-1] :
-            #verifica se o valor eh igual a pass
-            if isinstance(node.body[0], ast.Pass):
-                #caso seja incrementa o contador em 1
-                self.counter = self.counter + 1
-                #verifica se a funcao ja esta salva, caso sim incrementa o contador, caso nao adiciona no dic
-                if NomeDeFunc not in DicionarioDeNomedeFunc :
-                    DicionarioDeNomedeFunc.update({NomeDeFunc : 1})
-                else :
-                    DicionarioDeNomedeFunc.update({NomeDeFunc : DicionarioDeNomedeFunc[NomeDeFunc]+1})
-
-        
-    def visit_ClassDef(self, node) :
-        global Contador
-        global NomeDeClass
-        NomeDeClass = node.name
-        Contador = 0
-        for x in node.body :
-            if isinstance(x, ast.FunctionDef):
-                Contador+=1
-        super(MyCustomVisitor, self).generic_visit(node)
-
-class RodarAnalise() :
-    def __init__ (self) :
-        pass
-
-    
+   
 if __name__ == "__main__":
     from pydriller import GitRepository
     import re
@@ -111,6 +22,7 @@ if __name__ == "__main__":
     for lista in RepositoryMining('https://github.com/spulec/moto', only_modifications_with_file_types=['.py']).traverse_commits():
         jasalvou=False
         for modification in lista.modifications:
+            achou == False
             if modification.filename.endswith('.py') :
                 auxiliar=""
                 diff = modification.diff
@@ -131,132 +43,132 @@ if __name__ == "__main__":
 	                        				pass
 	                        			else:
 	                        				os.makedirs(pasta) # aqui criamos a pasta caso nao exista
-	                        				nomedotxt = pasta + lista.project_name+".csv"
-	                        				if os.path.isfile(nomedotxt) :
-	                        					arq = open(nomedotxt, 'a')
-	                        					arq.write(modification.filename)
-	                        					arq.write(";")
-	                        					arq.write(";")
-	                        					arq.write("ADD")
-	                        					arq.write(";")
-	                        					arq.write(lista.author.name)
-	                        					arq.write(";")
-	                        					arq.write(lista.author.email)
-	                        					arq.write(";")
-	                        					arq.write(str(lista.author_date))
-	                        					arq.write(";")
-	                        					arq.write(lista.hash)
-	                        					if jasalvou == False:
-	                        						arq.write(";")
-	                        						arq.write('"'+lista.msg.replace("\n", " ")+'"'+"\n")
-	                        						jasalvou=True
-	                        					else:
-	                        						arq.write(";")
-	                        						arq.write('""'+"\n")
-	                        					arq.close()
-	                        				else:
-	                        					arq = open(nomedotxt, 'w')
-	                        					arq.write("Filename")
-	                        					arq.write(";")
-	                        					arq.write("Linha")
-	                        					arq.write(";")
-	                        					arq.write("Tipo")
-	                        					arq.write(";")
-	                        					arq.write("Autor")
-	                        					arq.write(";")
-	                        					arq.write("Email")
-	                        					arq.write(";")
-	                        					arq.write("Data")
-	                        					arq.write(";")
-	                        					arq.write("Hash")
-	                        					arq.write(";")
-	                        					arq.write("Commit\n")
-	                        					arq.write(modification.filename)
-	                        					arq.write(";")
-	                        					arq.write(";")
-	                        					arq.write("ADD")
-	                        					arq.write(";")
-	                        					arq.write(lista.author.name)
-	                        					arq.write(";")
-	                        					arq.write(lista.author.email)
-	                        					arq.write(";")
-	                        					arq.write(str(lista.author_date))
-	                        					arq.write(";")
-	                        					arq.write(lista.hash)
+	                        			nomedotxt = pasta + lista.project_name+".csv"
+	                        			if os.path.isfile(nomedotxt) :
+	                        				arq = open(nomedotxt, 'a')
+	                        				arq.write(modification.filename)
+	                        				arq.write(";")
+	                        				arq.write(";")
+	                        				arq.write("ADD")
+	                        				arq.write(";")
+	                        				arq.write(lista.author.name)
+	                        				arq.write(";")
+	                        				arq.write(lista.author.email)
+	                        				arq.write(";")
+	                        				arq.write(str(lista.author_date))
+	                        				arq.write(";")
+	                        				arq.write(lista.hash)
+	                        				if jasalvou == False:
 	                        					arq.write(";")
 	                        					arq.write('"'+lista.msg.replace("\n", " ")+'"'+"\n")
-	                        					arq.close()
 	                        					jasalvou=True
-	                        				Salvarlista.append(testestring)
-	                        				achou=False;
-	                        				testestring=""
+	                        				else:
+	                        					arq.write(";")
+	                        					arq.write('""'+"\n")
+	                        				arq.close()
+	                        			else:
+	                        				arq = open(nomedotxt, 'w')
+	                        				arq.write("Filename")
+	                        				arq.write(";")
+	                        				arq.write("Linha")
+	                        				arq.write(";")
+	                        				arq.write("Tipo")
+	                        				arq.write(";")
+	                        				arq.write("Autor")
+	                        				arq.write(";")
+	                        				arq.write("Email")
+	                        				arq.write(";")
+	                        				arq.write("Data")
+	                        				arq.write(";")
+	                        				arq.write("Hash")
+	                        				arq.write(";")
+	                        				arq.write("Commit\n")
+	                        				arq.write(modification.filename)
+	                        				arq.write(";")
+	                        				arq.write(";")
+	                        				arq.write("ADD")
+	                        				arq.write(";")
+	                        				arq.write(lista.author.name)
+	                        				arq.write(";")
+	                        				arq.write(lista.author.email)
+	                        				arq.write(";")
+	                        				arq.write(str(lista.author_date))
+	                        				arq.write(";")
+	                        				arq.write(lista.hash)
+	                        				arq.write(";")
+	                        				arq.write('"'+lista.msg.replace("\n", " ")+'"'+"\n")
+	                        				arq.close()
+	                        				jasalvou=True
+	                        			Salvarlista.append(testestring)
+	                        			achou=False;
+	                        			testestring=""
                         			else:
                         				achou=False;
                         				testestring=""
                         		else:
                         			if testestring.endswith("pass"):
-                        				Salvartestestring.append(testestring)
+                        				Salvarlista.append(testestring)
                         				pasta = 'Testes/spulec/'+lista.project_name+"/"
 	                        			if os.path.isdir(pasta): # vemos de este diretorio ja existe
 	                        				pass
 	                        			else:
 	                        				os.makedirs(pasta) # aqui criamos a pasta caso nao exista
-	                        				nomedotxt = pasta + lista.project_name+".csv"
-	                        				if os.path.isfile(nomedotxt) :
-	                        					arq = open(nomedotxt, 'a')
-	                        					arq.write(modification.filename)
-	                        					arq.write(";")
-	                        					arq.write(";")
-	                        					arq.write("DELETED")
-	                        					arq.write(";")
-	                        					arq.write(lista.author.name)
-	                        					arq.write(";")
-	                        					arq.write(lista.author.email)
-	                        					arq.write(";")
-	                        					arq.write(str(lista.author_date))
-	                        					arq.write(";")
-	                        					arq.write(lista.hash)
-	                        					if jasalvou == False:
-	                        						arq.write(";")
-	                        						arq.write('"'+lista.msg.replace("\n", " ")+'"'+"\n")
-	                        						jasalvou=True
-	                        					else:
-	                        						arq.write(";")
-	                        						arq.write('""'+"\n")
-	                        					arq.close()
-	                        				else:
-	                        					arq = open(nomedotxt, 'w')
-	                        					arq.write("Filename")
-	                        					arq.write(";")
-	                        					arq.write("Linha")
-	                        					arq.write(";")
-	                        					arq.write("Tipo")
-	                        					arq.write(";")
-	                        					arq.write("Autor")
-	                        					arq.write(";")
-	                        					arq.write("Email")
-	                        					arq.write(";")
-	                        					arq.write("Data")
-	                        					arq.write(";")
-	                        					arq.write("Hash")
-	                        					arq.write(";")
-	                        					arq.write("Commit\n")
-	                        					arq.write(modification.filename)
-	                        					arq.write(";")
-	                        					arq.write(";")
-	                        					arq.write("DELETED")
-	                        					arq.write(";")
-	                        					arq.write(lista.author.name)
-	                        					arq.write(";")
-	                        					arq.write(lista.author.email)
-	                        					arq.write(";")
-	                        					arq.write(str(lista.author_date))
-	                        					arq.write(";")
-	                        					arq.write(lista.hash)
+	                        			nomedotxt = pasta + lista.project_name+".csv"
+	                        			if os.path.isfile(nomedotxt) :
+	                        				arq = open(nomedotxt, 'a')
+	                        				arq.write(modification.filename)
+	                        				arq.write(";")
+	                        				arq.write(";")
+	                        				arq.write("DELETED")
+	                        				arq.write(";")
+	                        				arq.write(lista.author.name)
+	                        				arq.write(";")
+	                        				arq.write(lista.author.email)
+	                        				arq.write(";")
+	                        				arq.write(str(lista.author_date))
+	                        				arq.write(";")
+	                        				arq.write(lista.hash)
+	                        				if jasalvou == False:
 	                        					arq.write(";")
 	                        					arq.write('"'+lista.msg.replace("\n", " ")+'"'+"\n")
-	                        					arq.close()
 	                        					jasalvou=True
+	                        				else:
+	                        					arq.write(";")
+	                        					arq.write('""'+"\n")
+	                        				arq.close()
+	                        			else:
+	                        				arq = open(nomedotxt, 'w')
+	                        				arq.write("Filename")
+	                        				arq.write(";")
+	                        				arq.write("Linha")
+	                        				arq.write(";")
+	                        				arq.write("Tipo")
+	                        				arq.write(";")
+	                        				arq.write("Autor")
+	                        				arq.write(";")
+	                        				arq.write("Email")
+	                        				arq.write(";")
+	                        				arq.write("Data")
+	                        				arq.write(";")
+	                        				arq.write("Hash")
+	                        				arq.write(";")
+	                        				arq.write("Commit\n")
+	                        				arq.write(modification.filename)
+	                        				arq.write(";")
+	                        				arq.write(";")
+	                        				arq.write("DELETED")
+	                        				arq.write(";")
+	                        				arq.write(lista.author.name)
+	                        				arq.write(";")
+	                        				arq.write(lista.author.email)
+	                        				arq.write(";")
+	                        				arq.write(str(lista.author_date))
+	                        				arq.write(";")
+	                        				arq.write(lista.hash)
+	                        				arq.write(";")
+	                        				arq.write('"'+lista.msg.replace("\n", " ")+'"'+"\n")
+	                        				arq.close()
+	                        				jasalvou=True
                         				achou=False;
                         				testestring=""
                         			else:
